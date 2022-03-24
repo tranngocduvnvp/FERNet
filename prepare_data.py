2a704c4e7db7961f7be45b39149df02bbe871f97
tranngocduvnvp <96320781+tranngocduvnvp@users.noreply.github.com>
2022-03-24 14:58:46 +0700

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -6,5 +6,5 @@ if not os.path.isdir('datasets'):
 
     url = 'https://www.dropbox.com/s/tntubvc72bilk1q/fer2013.csv?dl=1'
     r = requests.get(url, allow_redirects=True)
-    open('Data/fer2013/fer2013.csv', 'wb').write(r.content)
+    open('datasets/fer2013/fer2013.csv', 'wb').write(r.content)
     print("Success download data")
